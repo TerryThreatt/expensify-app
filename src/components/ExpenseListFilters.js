a539b0bf1ad83d868fadaf6a85fe4be2b5140f9e
Terry Threatt <terry.threatt@gmail.com>
2019-10-11 01:18:18 -0500

    add date-picker and updated editExpensePage component

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { setTextFilter } from '../actions/filters'
+import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters'
 
 
 const ExpenseListFilters = (props) => ( 
     <div>
-        <input type="text" value={props.filters.text} onChange={(e) => { 
+        <input 
+        type="text" 
+        value={ props.filters.text } 
+        onChange={(e) => { 
             props.dispatch(setTextFilter(e.target.value))
-        }} />     
+        }} /> 
+        <select 
+        value={ props.filters.sortBy} 
+        onChange={(e) => { 
+            if(e.target.value === "date") { 
+                props.dispatch(sortByDate(e.target.value))
+            } else if (e.target.value === "amount") { 
+                props.dispatch(sortByAmount(e.target.value))
+            }
+        }}>
+            <option value="date">Date</option>
+            <option value="amount">Amount</option>
+        </select>    
     </div>
 )
 
